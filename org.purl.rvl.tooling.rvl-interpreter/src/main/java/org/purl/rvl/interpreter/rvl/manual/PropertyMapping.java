49ec2b1a158dd67ae79728f8321470938ca15143
sarfarazaccenture <sarfarazaccenture@gmail.com>
2013-11-15 01:11:31 +0100

    b

--- org.purl.rvl.tooling.rvl-interpreter/src/main/java/org/purl/rvl/interpreter/rvl/manual/PropertyMapping.java
+++ org.purl.rvl.tooling.rvl-interpreter/src/main/java/org/purl/rvl/interpreter/rvl/manual/PropertyMapping.java
@@ -11,7 +11,7 @@ import org.ontoware.rdfreactor.schema.rdfs.Property;
 public class PropertyMapping extends
 		org.purl.rvl.interpreter.rvl.PropertyMapping {
 
-	static final String NL =  System.getProperty("line.separator");
+static final String NL =  System.getProperty("line.separator");
 	
 	public PropertyMapping(Model model, URI classURI,
 			Resource instanceIdentifier, boolean write) {
