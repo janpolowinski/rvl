74037452de0fc3975663db09588f0c0b8e50c89b
janpolowinski <jan.polowinski@gmail.com>
2014-04-06 00:23:00 +0200

    improved named graph experiments

--- org.purl.rvl.tooling.rvl-interpreter/src/test/java/org/purl/rvl/interpreter/test/ModelRepoTest.java
+++ org.purl.rvl.tooling.rvl-interpreter/src/test/java/org/purl/rvl/interpreter/test/ModelRepoTest.java
@@ -19,6 +19,7 @@ import org.ontoware.rdf2go.RDF2Go;
 import org.ontoware.rdf2go.Reasoning;
 import org.ontoware.rdf2go.impl.jena.ModelFactoryImpl;
 import org.ontoware.rdf2go.impl.jena.ModelSetImplJena;
+import org.ontoware.rdf2go.model.Diff;
 import org.ontoware.rdf2go.model.Model;
 import org.ontoware.rdf2go.model.ModelSet;
 import org.ontoware.rdf2go.model.QueryResultTable;
@@ -31,6 +32,7 @@ import org.ontoware.rdf2go.model.node.Variable;
 import org.ontoware.rdf2go.model.node.impl.URIImpl;
 import org.ontoware.rdf2go.vocabulary.RDF;
 import org.ontoware.rdfreactor.schema.rdfs.Property;
+import org.purl.rvl.java.gen.rvl.PropertyMapping;
 import org.purl.rvl.tooling.process.ExampleData;
 import org.purl.rvl.tooling.process.ExampleMapping;
 import org.purl.rvl.tooling.process.OGVICProcess;
@@ -38,8 +40,11 @@ import org.purl.rvl.tooling.util.CustomRecordFormatter;
 
 public class ModelRepoTest {
 	
+	private static final URIImpl GRAPH_RVL = new URIImpl("http://purl.org/rvl/");
 	private static final URIImpl GRAPH_MAPPING = new URIImpl("http://purl.org/rvl/example/mapping/");
 	private static final URIImpl GRAPH_DATA = new URIImpl("http://purl.org/rvl/example/data/");
+	private static final URIImpl GRAPH_MAPPING_ENRICHED_WITH_RVL = new URIImpl("http://purl.org/rvl/example/mapping/enriched/");
+	
 
 	private final static Logger LOGGER = Logger.getLogger(ModelRepoTest.class.getName()); 
 	private final static Logger LOGGER_RVL_PACKAGE = Logger.getLogger("org.purl.rvl"); 
@@ -70,7 +75,7 @@ public class ModelRepoTest {
 	 */
 	public static void main(String[] args) {
 		
-		// we use sesame here, since jena seems not to support SPARQL queries on model sets
+		// we use sesame here, since jena seems not to properly support SPARQL queries on model sets (e.g. named graph queries caused problems)
 		//RDF2Go.register( new org.ontoware.rdf2go.impl.jena.ModelFactoryImpl() );
 		RDF2Go.register( new org.openrdf.rdf2go.RepositoryModelFactory());
 		
@@ -88,28 +93,136 @@ public class ModelRepoTest {
 		readFromAnySyntax(dataModel, ExampleData.SLUB_TEST);
 		
 		// mapping
-		Model mappingModel = RDF2Go.getModelFactory().createModel(Reasoning.none);
+		Model mappingModel = RDF2Go.getModelFactory().createModel(Reasoning.rdfs);
 		mappingModel.open();
 		readFromAnySyntax(mappingModel, ExampleMapping.SLUB );
+		LOGGER.finest("mapping model size: " + mappingModel.size());
+
+		/*
+		System.out.println("Property mappings in the orig mapping model: ");
+
+		ClosableIterator<Statement> mappingModelIt = mappingModel.iterator();
+
+		while (mappingModelIt.hasNext()) {
+			
+			Statement pmStmt = (Statement) mappingModelIt.next();
+			
+			System.out.println(pmStmt);
+			
+		}*/
+		
+		// mapping inferred
+		Model inferredMappingModel = RDF2Go.getModelFactory().createModel(Reasoning.rdfs);
+		inferredMappingModel.open();
+		inferredMappingModel.addModel(mappingModel);
+		LOGGER.finest("inferred mapping model size: " + inferredMappingModel.size());
 		
 		// rvl
-		Model rvlModel = RDF2Go.getModelFactory().createModel(Reasoning.none);
+		Model rvlModel = RDF2Go.getModelFactory().createModel(Reasoning.rdfs);
 		rvlModel.open();
 		readFromAnySyntax(rvlModel, OGVICProcess.RVL_LOCAL_REL );
+		LOGGER.finest("rvl model size: " + rvlModel.size());
+		
+		Model inferredRVLModel = RDF2Go.getModelFactory().createModel(Reasoning.none);
+		inferredRVLModel.open();
+		inferredRVLModel.addModel(rvlModel);
+		LOGGER.finest("inferred rvl model size: " + inferredRVLModel.size());
+		
+		// mapping + rvl
+		Model mappingsAndRVLModel = RDF2Go.getModelFactory().createModel(Reasoning.rdfs);
+		mappingsAndRVLModel.open();
+		mappingsAndRVLModel.addModel(inferredRVLModel);
+		mappingsAndRVLModel.addModel(mappingModel);
+		LOGGER.finest("mappings + rvl model size: " + mappingsAndRVLModel.size());
+		
+		Model inferredMappingsAndRVLModel = RDF2Go.getModelFactory().createModel(Reasoning.none);
+		inferredMappingsAndRVLModel.open();
+		inferredMappingsAndRVLModel.addModel(mappingsAndRVLModel);
+		LOGGER.finest("inferred mappings + rvl model size: " + inferredMappingsAndRVLModel.size());
+		
+		Diff diff = rvlModel.getDiff(inferredMappingsAndRVLModel.iterator());
+		
+		Iterable<Statement> addedIt = diff.getAdded();
+
+		
+		// enriched mappings
+		Model enrichedMappings = RDF2Go.getModelFactory().createModel(Reasoning.rdfs);
+		enrichedMappings.open();
+		enrichedMappings.addModel(inferredMappingModel);
+		enrichedMappings.addAll(addedIt.iterator());
+
+		int i = 0;
+		for (Statement statement : addedIt) {
+			i++;
+			LOGGER.finest("diff : added statement ("+ i +"): " + statement );
+			
+		}
+		
+		Iterable<Statement> removedIt = diff.getRemoved();
+		
+		for (Statement statement : removedIt) {
+			LOGGER.finest("diff : removed statement: " + statement );
+		}
+		
+		LOGGER.finest("enriched mappings model size: " + enrichedMappings.size());
+		
+		
 		
 		modelSet.addModel(dataModel, GRAPH_DATA);
 		modelSet.addModel(mappingModel, GRAPH_MAPPING);
-		//modelSet.addModel(rvlModel, new URIImpl("http://purl.org/rvl/"));
+		modelSet.addModel(rvlModel, GRAPH_RVL);
+		modelSet.addModel(enrichedMappings, GRAPH_MAPPING_ENRICHED_WITH_RVL);
 		
 		
 		
+		
+
+		
+		
+		System.out.println("Property mappings in the enriched mapping model: ");
+		
+		// find stmts filtered
+		ClosableIterator<Statement> iteratorPM = mappingsAndRVLModel.findStatements(
+				Variable.ANY,
+				RDF.type,
+				PropertyMapping.RDFS_CLASS
+				);
+
+		while (iteratorPM.hasNext()) {
+			
+			Statement pmStmt = (Statement) iteratorPM.next();
+			
+			System.out.println(pmStmt);
+			
+		}
+		
+		System.out.println("Property mappings in the model sets enriched mapping graph: ");
+		
+		// find stmts filtered
+		ClosableIterator<Statement> iteratorPMInModelSet = modelSet.findStatements(
+				GRAPH_MAPPING_ENRICHED_WITH_RVL,
+				Variable.ANY,
+				RDF.type,
+				PropertyMapping.RDFS_CLASS
+				);
+
+		while (iteratorPMInModelSet.hasNext()) {
+			
+			Statement pmStmt = (Statement) iteratorPMInModelSet.next();
+			
+			System.out.println(pmStmt);
+			
+		}
+		
+
 		//listModelStatements("data model",dataModel);
 		//printModelSet(modelSet);
 		
 		/*
+		
 		// find stmts filtered
 		ClosableIterator<Statement> iterator = modelSet.findStatements(
-				new URIImpl("http://purl.org/rvl/example/data/"),
+				GRAPH_DATA,
 				Variable.ANY,
 				Variable.ANY,
 				Variable.ANY
@@ -125,6 +238,7 @@ public class ModelRepoTest {
 		
 		*/
 		
+		/*
 		// sparql-query filtered
 		
 		LOGGER.finest("Size of model set: " + modelSet.size());
@@ -143,7 +257,7 @@ public class ModelRepoTest {
 			
 			System.out.println(statement.getContext() + ": " + statement);
 			
-		}
+		} */
 		
 	}
 	
