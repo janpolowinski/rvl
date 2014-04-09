412f68331aa42cd78b085ae96bdc43d7cd8989d6
janpolowinski <jan.polowinski@gmail.com>
2014-04-09 19:22:04 +0200

    replace own RDF vocab class by extension of rdf2go's

--- org.purl.rvl.tooling.rvl-interpreter/src/main/java/org/purl/rvl/java/RDF.java
+++ org.purl.rvl.tooling.rvl-interpreter/src/main/java/org/purl/rvl/java/RDF.java
@@ -1,9 +1,9 @@
 package org.purl.rvl.java;
 
-public class RDF {
+public class RDF  extends org.ontoware.rdf2go.vocabulary.RDF {
 	
-	public static final String NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+	//public static final String NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 	
-	public static final String ID = NS + "ID";
+	public static final String ID = RDF_NS + "ID";
 	
 }
