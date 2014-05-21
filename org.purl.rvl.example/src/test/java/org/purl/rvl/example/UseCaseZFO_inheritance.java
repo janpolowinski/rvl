da425f4c5ebd292547346c350a7e3f58f4ae1cca
janpolowinski <Jan@Jans-iMac.local>
2014-05-21 19:42:24 +0200

    adding owl test

--- org.purl.rvl.example/src/test/java/org/purl/rvl/example/UseCaseZFO_inheritance.java
+++ org.purl.rvl.example/src/test/java/org/purl/rvl/example/UseCaseZFO_inheritance.java
@@ -3,6 +3,7 @@ package org.purl.rvl.example;
 import java.io.FileNotFoundException;
 
 import org.junit.Test;
+import org.ontoware.rdf2go.Reasoning;
 import org.purl.rvl.interpreter.test.TestOGVICProcess;
 import org.purl.rvl.tooling.avm2d3.D3GeneratorSimpleJSON;
 import org.purl.rvl.tooling.avm2d3.D3GeneratorTreeJSON;
@@ -14,13 +15,15 @@ public class UseCaseZFO_inheritance extends TestOGVICProcess {
 	@Test
 	public void testOGVICProcess() throws FileNotFoundException {
 
+		project.setReasoningDataModel(Reasoning.rdfsAndOwl);
+		
 		project.registerMappingFile(ExampleMapping.ZFO_inheritance);
 		project.registerDataFile(ExampleData.ZFO_SUBSET);
 		//project.registerDataFile(ExampleData.ZFO);
 		
 		//project.setRvlInterpreter(new SimpleRVLInterpreter());
-		project.setD3Generator(new D3GeneratorSimpleJSON()); // requires better filtering!
-		//project.setD3Generator(new D3GeneratorTreeJSON());
+		//project.setD3Generator(new D3GeneratorSimpleJSON()); // requires better filtering!
+		project.setD3Generator(new D3GeneratorTreeJSON());
 		
 		loadProjectAndRunProcess();
 	}
