597cae4526635bc7e3eea05acc5d7c7a5b142ba8
janpolowinski <Jan@Jans-iMac.local>
2014-05-22 18:32:35 +0200

    setting use case reasoning

--- org.purl.rvl.example/src/test/java/org/purl/rvl/example/UseCasePO_8.java
+++ org.purl.rvl.example/src/test/java/org/purl/rvl/example/UseCasePO_8.java
@@ -3,28 +3,41 @@ package org.purl.rvl.example;
 import java.io.FileNotFoundException;
 
 import org.junit.Test;
+import org.ontoware.rdf2go.Reasoning;
 import org.purl.rvl.interpreter.test.TestOGVICProcess;
+import org.purl.rvl.tooling.avm2d3.D3GeneratorSimpleJSON;
 import org.purl.rvl.tooling.avm2d3.D3GeneratorTreeJSON;
 import org.purl.rvl.tooling.process.ExampleData;
 import org.purl.rvl.tooling.process.ExampleMapping;
 
 public class UseCasePO_8 extends TestOGVICProcess {
-	
+
 	@Test
 	public void testOGVICProcess() throws FileNotFoundException {
-		
-		//process.setUriStart("http://purl.org/obo/owl/");
+
+		// process.setUriStart("http://purl.org/obo/owl/");
+
+		project.setReasoningDataModel(Reasoning.none); // setting to rdfs leads
+														// to strange results
+														// and many statements
+														// are inferred and
+														// displayed (since
+														// mostSpecific does not
+														// apply to sub-classing
+														// but ont sub-property
+														// hierarchies), but
+														// shape not evaluated
+														// otherwise
 
 		project.registerMappingFile(ExampleMapping.PO_8);
 		project.registerDataFile(ExampleData.PO);
 		project.registerDataFile(ExampleData.PO_EXTRA_DATA);
-		
-		//project.setRvlInterpreter(new SimpleRVLInterpreter());
-		project.setD3Generator(new D3GeneratorTreeJSON());
-		//process.setD3Generator(new D3GeneratorSimpleJSON());
-		
+
+		// project.setRvlInterpreter(new SimpleRVLInterpreter());
+		// project.setD3Generator(new D3GeneratorTreeJSON());
+		process.setD3Generator(new D3GeneratorSimpleJSON());
+
 		loadProjectAndRunProcess();
 	}
-
 
 }
--- org.purl.rvl.example/src/test/java/org/purl/rvl/example/UseCasePO_9.java
+++ org.purl.rvl.example/src/test/java/org/purl/rvl/example/UseCasePO_9.java
@@ -3,6 +3,7 @@ package org.purl.rvl.example;
 import java.io.FileNotFoundException;
 
 import org.junit.Test;
+import org.ontoware.rdf2go.Reasoning;
 import org.purl.rvl.interpreter.test.TestOGVICProcess;
 import org.purl.rvl.tooling.avm2d3.D3GeneratorTreeJSON;
 import org.purl.rvl.tooling.process.ExampleData;
@@ -14,6 +15,8 @@ public class UseCasePO_9 extends TestOGVICProcess {
 	public void testOGVICProcess() throws FileNotFoundException {
 		
 		//process.setUriStart("http://purl.org/obo/owl/");
+		
+		project.setReasoningDataModel(Reasoning.rdfs); // setting to rdfs leads to strange results three additional statements that seems to be unrelated
 
 		project.registerMappingFile(ExampleMapping.PO_9);
 		project.registerDataFile(ExampleData.PO);
