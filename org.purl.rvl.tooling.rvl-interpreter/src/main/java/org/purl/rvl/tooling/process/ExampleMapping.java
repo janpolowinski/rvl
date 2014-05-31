fd427988e673d115854ce158bcea76f3fcfc8740
janpolowinski <jan.polowinski@gmail.com>
2014-05-31 16:41:28 +0200

    add new configurable labeling example

--- org.purl.rvl.tooling.rvl-interpreter/src/main/java/org/purl/rvl/tooling/process/ExampleMapping.java
+++ org.purl.rvl.tooling.rvl-interpreter/src/main/java/org/purl/rvl/tooling/process/ExampleMapping.java
@@ -31,6 +31,8 @@ public class ExampleMapping {
 	
 	public static final String RVL_EXAMPLE = "../org.purl.rvl.vocabulary/example-mappings/rvl-example-mappings.ttl";
 	public static final String RVL_EXAMPLE_FILTERING = "../org.purl.rvl.vocabulary/experiments/example-mappings/rvl-example-filtering.ttl";
+	public static final String RVL_EXAMPLE_LABELING = "../org.purl.rvl.vocabulary/example-mappings/labeling-test.ttl";
+
 	
 	// problem (no root nodes found) here probably because subClassOf etc. are reflexiv and our method for finding root nodes does not work here! -> exlude direct "loop" relations
 	public static final String RVL_EXAMPLE_BOOTSTRAP = "../org.purl.rvl.vocabulary/example-mappings/rvl-bootstrap.ttl";
