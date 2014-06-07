0e5dde2622ec78c3d4ff5b8b3daa435191ef4e64
janpolowinski <Jan@Jans-iMac.local>
2014-06-07 07:57:06 +0200

    remove unused members

--- org.purl.rvl.tooling.rvl-interpreter/src/main/java/org/purl/rvl/tooling/rvl2avm/MappingHandler.java
+++ org.purl.rvl.tooling.rvl-interpreter/src/main/java/org/purl/rvl/tooling/rvl2avm/MappingHandler.java
@@ -1,11 +1,7 @@
 package org.purl.rvl.tooling.rvl2avm;
 
-import org.purl.rvl.exception.InsufficientMappingSpecificationException;
-import org.purl.rvl.java.rvl.PropertyToGO2ORMappingX;
 
 public interface MappingHandler {
 
-	//public abstract void handleP2GOTORMapping(PropertyToGO2ORMappingX p2go2orm)
-	//		throws InsufficientMappingSpecificationException;
 
 }
--- org.purl.rvl.tooling.rvl-interpreter/src/main/java/org/purl/rvl/tooling/rvl2avm/MappingHandlerBase.java
+++ org.purl.rvl.tooling.rvl-interpreter/src/main/java/org/purl/rvl/tooling/rvl2avm/MappingHandlerBase.java
@@ -3,11 +3,12 @@
  */
 package org.purl.rvl.tooling.rvl2avm;
 
+import java.util.Iterator;
+
 import org.ontoware.rdf2go.model.Model;
 import org.ontoware.rdf2go.model.ModelSet;
+import org.ontoware.rdf2go.model.Statement;
 import org.purl.rvl.exception.InsufficientMappingSpecificationException;
-import org.purl.rvl.java.rvl.PropertyToGO2ORMappingX;
-import org.purl.rvl.tooling.process.OGVICProcess;
 
 /**
  * @author Jan Polowinski
@@ -17,13 +18,11 @@ public abstract class MappingHandlerBase implements MappingHandler  {
 	
 	protected ModelSet modelSet;
 	protected Model modelAVM;
-	protected Model modelData;
-	protected Model modelMappings;
-	protected Model modelVISO;
 	
 	protected RVLInterpreter rvlInterpreter;
 	
 	int processedGraphicRelations = 0;
+	protected Iterator<Statement> stmtSetIterator;
 	
 	public MappingHandlerBase(ModelSet modelSet, RVLInterpreter rvlInterpreter, Model modelAvm) {
 		super();
@@ -32,15 +31,7 @@ public abstract class MappingHandlerBase implements MappingHandler  {
 		this.modelAVM = modelAvm;
 	}
 
-	public void init(Model modelAVM, ModelSet modelSet) {
-			this.modelSet = modelSet;
-			//this.modelAVM = modelSet.getModel(OGVICProcess.GRAPH_AVM); // somehow the AVM model seems not to be available here
-			this.modelData = modelSet.getModel(OGVICProcess.GRAPH_DATA);
-			this.modelMappings = modelSet.getModel(OGVICProcess.GRAPH_MAPPING);
-			this.modelVISO = modelSet.getModel(OGVICProcess.GRAPH_VISO);
-		}
-
-
-
+	protected abstract void encodeStatement(Statement statement)
+			throws InsufficientMappingSpecificationException;
 
 }
