80058c65b231f14ab2711b20f823fed7b81b8f27
janpolowinski <Jan@jans-imac.fritz.box>
2014-06-24 13:57:33 +0200

    choose text color based on background color

--- org.purl.rvl.tooling.d3vis/js/ogvic.js
+++ org.purl.rvl.tooling.d3vis/js/ogvic.js
@@ -122,6 +122,7 @@ var width = 1400,
 					.selectAll(".textHTMLLabelContainer")
 					.data(function(d) { return d.labels ; }).enter()
 					.avmLabeledHTML2();
+					// filter .... here ...
 				
 				/*labelContainerContainer
 				.filter(function(d) { return d.type === "text_label" ;})
@@ -236,7 +237,14 @@ var width = 1400,
 		  
 		  containerDiv.append("div")
 		  			.attr("class", "htmlTextLabel")
-		  			.style("color", function(d){return d.color_rgb_hex_combined;})
+		  			.style("color", function(d){
+		  				var textColor = d.color_rgb_hex_combined;
+		  				var bgColor = this.parentNode.parentNode.__data__.color_rgb_hex_combined;
+		  				//var parentNode2 = this.parentNode.parentNode;
+		  				//console.log(parentNode2.__data__.color_rgb_hex_combined); // TODO why does parentNode2.data() not work???
+		  				if (textColor == null) textColor = calculateHighContrastColor(bgColor);
+		  				return textColor;
+		  				})
 					//.style("width", NODE_SIZE + "px") // does only work well for xCenter position
 					.html(function(d){ return d.text_value;});
 		  
@@ -683,5 +691,49 @@ function toggle(d) {
 		text(function(d){return d.text_value;});
  }
  
+ // TODO usage and calculation incomplete
+ function calculateHighContrastColor(backgroundColor) {
+	 
+	 var backgroundColorRGB = d3.rgb(backgroundColor);
+
+	 // this may be not the best formula
+	 
+	 // 0.2126 * (r/255)^2.2  +  0.7151 * (g/255)^2.2  +  0.0721 * (b/255)^2.2;
+	 /*var l1 = 
+		 0.2126 * Math.pow((backgroundColorRGB.r/255), 2.2) + 
+		 0.7151 * Math.pow((backgroundColorRGB.g/255), 2.2) + 
+		 0.0721 * Math.pow((backgroundColorRGB.b/255), 2.2);
+
+	   return contrastRatio <= 0.18 ? "#eee" : "#222";
+
+	  */
+		 
+	 // another from http://www.w3.org/TR/2014/NOTE-WCAG20-TECHS-20140408/G18
+	 
+	 function relativeLuminance(r,g,b) {
+		  return 0.2126 * r + 0.7152 * g + 0.0722 * b ;
+	 }
+	
+	 function luminanceLetter(s) {
+		return s <= 0.03928 ? s/12.92 : Math.pow(((s+0.055)/1.055), 2.4);
+	 }
+	 
+	 var rs = backgroundColorRGB.r/255, 
+	 	 gs = backgroundColorRGB.g/255, 
+	 	 bs = backgroundColorRGB.b/255;
+	 	
+	 var r = luminanceLetter(rs), g = luminanceLetter(gs), b = luminanceLetter(bs);
+	 var rBlack = luminanceLetter(0), gBlack = luminanceLetter(0), bBlack = luminanceLetter(0);
  
+	 var l1  =  relativeLuminance(r,g,b);
+	 var l2  =  relativeLuminance(rBlack,gBlack,bBlack);
+	 
+	 //(L1 + 0.05) / (L2 + 0.05), 
+	 var contrastRatio = (l1 + 0.05) / (l2 + 0.05); 
+	 
+	 // sth. seems to be wrong with this one as well (cf. RO-4b):
+	 return contrastRatio >= 4.5 ? "black" : "white"; 
+ }
+
+
  
