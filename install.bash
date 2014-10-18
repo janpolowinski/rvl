d01d4cd602d71f77d3d333b76be655e21350cf8a
janpolowinski <jan.polowinski@gmail.com>
2014-10-18 21:15:48 +0200

    fix wrong module name in script

--- install.bash
+++ install.bash
@@ -6,7 +6,7 @@ cd ../org.purl.rvl.tooling.commons
 mvn install -DskipTests
 cd ../org.purl.rvl.tooling.codegen.rdfreactor
 mvn install -DskipTests
-cd ../org.purl.rvl.tooling.rvl-interpreter
+cd ../org.purl.rvl.tooling.interpreter
 mvn install -DskipTests
 cd ../org.purl.rvl.tooling.d3vis
 mvn install -DskipTests
