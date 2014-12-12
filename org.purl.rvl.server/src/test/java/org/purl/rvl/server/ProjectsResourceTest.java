fabbd75668c0fda3f9820679e84ab133f8700b59
janpolowinski <jan.polowinski@gmail.com>
2014-12-12 09:24:17 +0100

    test posting multipart/form-data

--- org.purl.rvl.server/src/test/java/org/purl/rvl/server/ProjectsResourceTest.java
+++ org.purl.rvl.server/src/test/java/org/purl/rvl/server/ProjectsResourceTest.java
@@ -4,19 +4,61 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
 import org.apache.commons.io.IOUtils;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 import org.purl.rvl.exception.OGVICRepositoryException;
 import org.purl.rvl.tooling.avm2d3.GraphicType;
 import org.purl.rvl.tooling.commons.utils.FileResourceUtils;
 import org.purl.rvl.tooling.process.VisProject;
 
-public class ProjectsResourceTest {
+public class ProjectsResourceTest extends JerseyTest {
+	
+    @Override
+    protected URI getBaseUri() {
+        return UriBuilder.fromUri(super.getBaseUri()).path("").build();
+    }
+
+    @Override
+    protected Application configure() {
+        return new RVLServerResourceConfiguration();
+    }
+
+    @Override
+    protected void configureClient(ClientConfig config) {
+        config.register(MultiPartFeature.class);
+    }
+	
+    @Test
+    public void testFormDataMultiPart() {
+    	
+    	//URI uri = getBaseUri();
+    	
+        final WebTarget target = target().path("projects/run");
+
+        final FormDataMultiPart mp = new FormDataMultiPart();
+        final FormDataBodyPart p = new FormDataBodyPart(FormDataContentDisposition.name("id").build(), "test-project-id");
+        mp.bodyPart(p);
+
+        final String s = target.request().post(Entity.entity(mp, MediaType.MULTIPART_FORM_DATA_TYPE), String.class);
+        assertEquals("{\"some message\" : \"test\"}", s);
+    }
 	
 	@Test
 	public void testCreatingTempFile() throws IOException {
@@ -38,7 +80,7 @@ public class ProjectsResourceTest {
 		String mappings = IOUtils.toString(mappingReader);
 		
 		ProjectsResource res = new ProjectsResource();
-		Response response = res.runNewVisProject("test", GraphicType.FORCE_DIRECTED_GRAPH, data, mappings, "on", null);
+		Response response = res.runNewVisProject("test", GraphicType.FORCE_DIRECTED_GRAPH, data, mappings, null);
 		
 		System.out.println(response);
 	}
