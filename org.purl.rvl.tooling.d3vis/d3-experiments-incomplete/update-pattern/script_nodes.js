ba0349e2ee42d557617f782b45e84ed1236325a1
janpolowinski <jan.polowinski@gmail.com>
2015-02-11 20:09:15 +0100

    close but all objects positioned the same

--- org.purl.rvl.tooling.d3vis/d3-experiments-incomplete/update-pattern/script_nodes.js
+++ org.purl.rvl.tooling.d3vis/d3-experiments-incomplete/update-pattern/script_nodes.js
@@ -1,21 +1,98 @@
     var graph;
+    var myNodes = [];
+    
+    
+    var containsArrayThisElement = function (arrayToCheck, element) {
+	    var containedElement = null;
+	    for (var i = 0, len = arrayToCheck.length; i < len; ++i) {
+	        if (arrayToCheck[i].uri==element.uri) {
+	        	containedElement = arrayToCheck[i];
+	        }
+		}
+	    return containedElement;
+	}
+    
+    var copyForceSettings = function (positionedObject, newObject) {
+		   
+    	newObject.index = positionedObject.index ; 
+    	newObject.x = positionedObject.x ; 
+    	newObject.y = positionedObject.y ; 
+    	newObject.px = positionedObject.px ; 
+    	newObject.py = positionedObject.py ; 
+    	newObject.fixed = positionedObject.fixed ; 
+    	newObject.weight = positionedObject.weight ; 
+//    	
+//    	from the docu at https://github.com/mbostock/d3/wiki/Force-Layout:
+//        index - the zero-based index of the node within the nodes array.
+//        x - the x-coordinate of the current node position.
+//        y - the y-coordinate of the current node position.
+//        px - the x-coordinate of the previous node position.
+//        py - the y-coordinate of the previous node position.
+//        fixed - a boolean indicating whether node position is locked.
+//        weight - the node weight; the number of associated links.
+	    return newObject;
+	} 
+    
     function myGraph() {
 
         // Add and remove elements on the graph object
         this.addNode = function (id) {
-            nodes.push({"id": id});
+        	myNodes.push({"id": id, "text" : id});
             update();
         };
         
         // Add and remove elements on the graph object
         this.replaceNodes = function () {
         	//alert(nodes);
-        	var newNodes = [{"id": "test1"},{"id": "Emma"},{"id": "Alex"}];
-            force.nodes(newNodes);
-            nodes = force.nodes();
+        	var newNodes = [{"id": "test", "text" : "testr"},{"id": "Emma", "text" : "Emmar"},{"id": "Alex", "text" : "Alexr"}];
+
+    		myNodes.length = 0;
+    		
+    	    for (var i = 0, len = newNodes.length; i < len; ++i) {
+    	    	myNodes.push(newNodes[i]);
+    	    }
+    	    
             //alert(newNodes);
             update();
         };
+        
+        this.modifyNodes = function () {
+        	//alert(nodes);
+        	var newNodes = [{"id": "test", "text" : "test1"},{"id": "Emma", "text" : "Emma1"},{"id": "Alex", "text" : "Alex1"}];
+
+        	var result = [];
+
+    		if (myNodes.length==0) {
+    			result = newNodes;
+    		}
+    		else {
+    			for (var i = 0, len = newNodes.length; i < len; ++i) {
+    				
+    				var newElement = newNodes[i];
+    				var existingElement = containsArrayThisElement(myNodes, newElement);
+    				
+    		        if (null!=existingElement) {
+//    		            result.push(existingElement); // changes to the properties of this object will be lost!
+    		            result.push(copyForceSettings(existingElement, newElement)); // should copy all settings from the old force-positioned object -> problem: all objects get the same settings of one old object
+//    		            result.push(newElement);
+    		        } else {
+    		        	result.push(newElement); // OK: this new object will get defaults on next force tick
+    		        }
+    			}
+    		}
+
+    		
+    		myNodes.length = 0;
+    		
+    	    for (var i = 0, len = result.length; i < len; ++i) {
+    	    	myNodes.push(result[i]);
+    	    }
+    	    
+            //alert(newNodes);
+            update();
+        };
+        
+        
 
 
         // set up the D3 visualisation in the specified element
@@ -32,12 +109,22 @@
                 .append('svg:g');
 
         var force = d3.layout.force();
-        var nodes = force.nodes();
+        
+        force
+        .gravity(.01)
+        .charge(-80000)
+        .friction(0)
+        .linkDistance( function(d) { return d.value * 10 } )
+        .size([w, h]);
+        
+        force.nodes(myNodes);
+        
+        force.start();
 
         var update = function () {
 
             var node = vis.selectAll("g.node")
-                    .data(nodes, function (d) {
+                    .data(myNodes, function (d) {
                         return d.id;
                     });
 
@@ -49,9 +136,11 @@
                     .attr("class", "textClass")
                     .attr("x", 14)
                     .attr("y", ".31em")
-                    .text(function (d) {
-                        return d.id;
-                    });
+                    ;
+                
+            node.selectAll("text").text(function (d) {
+                    return d.text;
+                });
 
             node.exit().remove();
 
@@ -63,13 +152,8 @@
             });
 
             // Restart the force layout.
-            force
-                    .gravity(.01)
-                    .charge(-80000)
-                    .friction(0)
-                    .linkDistance( function(d) { return d.value * 10 } )
-                    .size([w, h])
-                    .start();
+            //force.start();
+            force.start();
         };
 
 
@@ -81,6 +165,7 @@
 
         graph = new myGraph("#svgdiv");
 
+        graph.addNode('test');
         graph.addNode('Emma');
         graph.addNode('Alex');
         
@@ -92,13 +177,17 @@
             return 2000 + (1500*step); // initial time, wait time
         }
         
-        setTimeout(function() {
+        /*setTimeout(function() {
         	 graph.addNode('Jan');
-        }, nextval());
+        }, nextval());*/
+        
+//        setTimeout(function() {
+//       	 graph.replaceNodes();
+//       }, nextval());
         
         setTimeout(function() {
-       	 graph.replaceNodes();
-       }, nextval());
+          	 graph.modifyNodes();
+          }, nextval());
 
     }
 
