983ddedca6d447d107466d59394efe598fafddc6
janpolowinski <jan.polowinski@gmail.com>
2015-02-13 16:48:39 +0100

    complex update example

---
+++ org.purl.rvl.tooling.d3vis/d3-experiments-incomplete/update-pattern/update_complex.js
@@ -0,0 +1,8 @@
+function list(ul, data) {
+  var li = ul.selectAll("li").data(data, function(d) {return d.id ;});
+  li.exit().remove();
+  li.enter().append("li");
+  li.text(function(d) { return d.text; });
+}
+
+//d3.select("#list").call(list, [0, 1, 2, 3, 4]);
