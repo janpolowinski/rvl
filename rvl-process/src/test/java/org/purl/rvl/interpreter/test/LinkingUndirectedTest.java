ce8c6d61352c84e67d95e59c4c7f183b93cda9d9
Jan Polowinski <jan.polowinski@gmail.com>
2015-09-03 15:30:06 +0200

    new linking undirected test

---
+++ rvl-process/src/test/java/org/purl/rvl/interpreter/test/LinkingUndirectedTest.java
@@ -0,0 +1,11 @@
+package org.purl.rvl.interpreter.test;
+
+
+public class LinkingUndirectedTest extends TestOGVICProcess {
+
+	@Override
+	protected String getProjectName() {
+		return "linking-undirected-test";
+	}
+	
+}
