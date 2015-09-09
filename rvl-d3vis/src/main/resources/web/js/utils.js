2393d4aee709561cb4562e02e47bf963ba8cbbce
Jan Polowinski <jan.polowinski@gmail.com>
2015-09-09 10:08:27 +0200

    browser warning for non-firefox

---
+++ rvl-d3vis/src/main/resources/web/js/utils.js
@@ -0,0 +1,6 @@
+function checkBrowser() {
+
+	if (navigator.userAgent.indexOf("Firefox") == -1) {
+		alert('Please use Firefox to experiment with this prototype for now. Complex labeling is not yet supported in browsers such as Chrome or Safari.');
+	} 
+}
