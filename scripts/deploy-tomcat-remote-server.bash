223a72dddb32df9a18e55c37d968217e1df43b72
Jan Polowinski <jan.polowinski@gmail.com>
2015-09-15 14:36:46 +0200

    fix wrong paths in script

--- scripts/deploy-tomcat-remote-server.bash
+++ scripts/deploy-tomcat-remote-server.bash
@@ -5,9 +5,6 @@ echo "Deploying to Tomcat and restarting ... (requires sudo)"
 sudo rm /opt/tomcat-base/webapps/semvis.war
 sudo rm -R /opt/tomcat-base/webapps/semvis
 
-cd target
-sudo cp -pr semvis.war /opt/tomcat-base/webapps
-
-cd ../../scripts
+sudo cp -pr ../rvl-server/target/semvis.war /opt/tomcat-base/webapps
 
 sudo service tomcat8 restart
