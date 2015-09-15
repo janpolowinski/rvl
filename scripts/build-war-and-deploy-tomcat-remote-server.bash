61879506260ac3e6785a3eb532a6e6f90cfc1ff1
Jan Polowinski <jan.polowinski@gmail.com>
2015-09-15 19:53:04 +0200

    remove sudo from script

--- scripts/build-war-and-deploy-tomcat-remote-server.bash
+++ scripts/build-war-and-deploy-tomcat-remote-server.bash
@@ -5,10 +5,10 @@ echo "Building war and deploying to Tomcat (without restarting, this may require
 cd ../rvl-server
 mvn war:war -DskipTests -U
 
-sudo rm /opt/tomcat-base/webapps/semvis.war
-sudo rm -R /opt/tomcat-base/webapps/semvis
+rm /opt/tomcat-base/webapps/semvis.war
+rm -R /opt/tomcat-base/webapps/semvis
 
-sudo cp -pr target/semvis.war /opt/tomcat-base/webapps
+cp -pr target/semvis.war /opt/tomcat-base/webapps
 
 # may require more rights
 # sudo service tomcat8 restart
