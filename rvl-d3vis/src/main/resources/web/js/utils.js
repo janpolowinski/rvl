d212980bb65933823f635128fc0817188043476e
Jan Polowinski <jan.polowinski@gmail.com>
2015-09-18 10:27:17 +0200

    pass default graphic type to editor

--- rvl-d3vis/src/main/resources/web/js/utils.js
+++ rvl-d3vis/src/main/resources/web/js/utils.js
@@ -1,6 +1,11 @@
 function checkBrowser() {
-
 	if (navigator.userAgent.indexOf("Firefox") == -1) {
 		alert('Please use Firefox to experiment with this prototype for now. Complex labeling is not yet supported in browsers such as Chrome or Safari.');
-	} 
+	}
+}
+
+function getProjectByID(projects, id) {
+	return projects.filter(function(project) {
+		return project.id === id;
+	})[0];
 }
